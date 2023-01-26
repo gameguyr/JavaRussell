b0e37464e2c6175d124e6eceb42e5d82e46871ad
Russell Lego <lego@adobe.com>
2023-01-26 09:16:04 -0800

    making first file for the berkeley java class

---
+++ src/main/java/aBeginnersGuide/Example.java
@@ -0,0 +1,10 @@
+package aBeginnersGuide;
+/*
+* This is a simple Java program
+* Call this file Example.java*/
+public class Example {
+    // a Java program begins with a call to main()
+    public static void main(String[] args) {
+        System.out.println("Java drives the web");
+    }
+}
